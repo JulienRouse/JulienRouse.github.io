8b3c6e014e53de4e7056a47ff62fc4910e96d766
JulienRouse <julien.rouse@gmail.com>
2018-01-03 17:17:18 +0100

    Change markdown directory to help translation, and change js for statcounter

--- template/statcounter.js
+++ template/statcounter.js
@@ -1,5 +1,5 @@
 <!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
+/*<script type="text/javascript">
 var sc_project=11577542;
 var sc_invisible=1;
 var sc_security="0efc2a5d";
@@ -14,4 +14,19 @@ href="http://statcounter.com/" target="_blank"><img
 class="statcounter"
 src="//c.statcounter.com/11577542/0/0efc2a5d/1/" alt="site
 stats"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
+*/
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=11577542;
+var sc_invisible=1;
+var sc_security="0efc2a5d";
+</script>
+<script type="text/javascript" async
+src="http://www.statcounter.com/counter/counter.js"></script>
+    <noscript><div class="statcounter"><a title="hits counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/11577542/0/0efc2a5d/1/"
+alt="hits counter"></a></div></noscript>
 <!-- End of StatCounter Code for Default Guide -->
